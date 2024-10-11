a6e037055ccda18d30dfc8d1a5076199c0a438b3
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2024-10-10 21:23:00 -0600

    Update eslint.config.mjs
    added jest configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
 ];
